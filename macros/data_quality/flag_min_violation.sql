4561ea389f57ae55de5aaff3dda24e54680b97e8
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-03-11 19:38:22 -0500

    break up DQ flagging macros

---
+++ macros/data_quality/flag_min_violation.sql
@@ -0,0 +1,22 @@
+{% macro flag_min_violation(target_col, min, where, inclusive=false, quote=false) %}
+
+    {% if quote==true %}
+        {% set _target_col = adapter.quote(target_col) %}
+        {% set _output_col = adapter.quote('flag_min_' + target_col) %}
+    {% else %}
+        {% set _target_col = target_col %}
+        {% set _output_col = 'flag_min_' + target_col %}
+    {% endif %}
+
+    {% if inclusive is false %}
+        {% set min_comparison = '<' %}
+    {% else %}
+        {% set min_comparison = '<=' %}
+    {% endif %}
+
+    case
+        when {{ _target_col }} {{min_comparison}} {{min}} {% if where is defined %} and {{where}} {% endif %} then 1
+        else 0
+    end as {{ _output_col }}
+
+{% endmacro %}
