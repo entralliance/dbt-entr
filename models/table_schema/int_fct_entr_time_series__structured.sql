18669cfc9538af4f2fdd450853b309316e2150bb
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-04-11 04:41:01 -0400

    update asset ID name in main fact table

--- models/table_schema/int_fct_entr_time_series__structured.sql
+++ models/table_schema/int_fct_entr_time_series__structured.sql
@@ -1,9 +1,14 @@
+{#
+    TODO:
+        - break up values into columns of different types (numeric, string, date/timestamp, (boolean?))
+#}
+
 with dummy_cte as (
     select 1 as foo
 )
 
 select
-    cast(null as {{dbt.type_int()}}) as plant_id,
+    cast(null as {{dbt.type_int()}}) as asset_id,
     cast(null as {{dbt.type_int()}}) as entr_tag_id,
     cast(null as {{dbt.type_timestamp()}}) as date_time,
     cast(null as {{dbt.type_numeric()}}) as tag_value,
